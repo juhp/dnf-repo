5540d7f847955bdfaf511baa314762289d332aba
Jens Petersen <juhpetersen@gmail.com>
2023-07-02 22:41:44 +0800

    Sudo: print cmd for debug

--- src/Sudo.hs
+++ src/Sudo.hs
@@ -8,8 +8,7 @@ import SimpleCmd (cmdN, sudo_)
 -- FIXME make this silent (simple-cmd-0.2.7) unless debug
 doSudo :: Bool -> Bool -> String -> [String] -> IO ()
 doSudo dryrun debug c args = do
-  if dryrun
-    then cmdN c args
-    else do
-    when debug $ cmdN c args
+  when (dryrun || debug) $ do
+    cmdN c args
+  unless dryrun $
     sudo_ c args
