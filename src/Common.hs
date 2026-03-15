38e6c4c27ba2ef26f9b1caf87d975a21e4c9b7d2
Jens Petersen <juhpetersen@gmail.com>
2026-03-15 14:27:30 +0800

    move getRpmOsRelease and getRpmOSName from CoprRepo to Common

--- src/Common.hs
+++ src/Common.hs
@@ -1,10 +1,51 @@
 module Common (
-  getSysArch
+  getSysArch,
+  getRpmOsRelease,
+  getRpmOSName
   )
 where
 
-import SimpleCmd (cmd)
+import Data.List.Extra (dropPrefix, dropSuffix, find, isPrefixOf)
+import SimpleCmd (cmd, cmdLines, cmdMaybe, error', grep, (+-+))
 
 getSysArch :: IO String
 getSysArch =
   cmd "rpm" ["--eval", "%{_arch}"]
+
+getRpmOsRelease :: IO String
+getRpmOsRelease = do
+  -- not defined for fedora branches
+  let systemReleaseVer = "system-release(releasever)"
+  mReleaseVerPkg <- cmdMaybe "rpm" ["-q", "--whatprovides", systemReleaseVer]
+  case mReleaseVerPkg of
+    Just relverPkg -> do
+      mreleasever <- find (systemReleaseVer `isPrefixOf`) <$> cmdLines "rpm" ["-q", "--provides", relverPkg]
+      case mreleasever of
+        Just releasever -> return $ last (words releasever)
+        Nothing -> error' $ "failed to determine" +-+ systemReleaseVer
+    Nothing -> do
+      let systemRelease = "system-release"
+      msysreleasepkg <- cmdMaybe "rpm" ["-q", "--whatprovides", systemRelease]
+      case msysreleasepkg of
+        Just sysrelpkg -> do
+          let prefix = systemRelease ++ "("
+          msysrelease <- find (prefix `isPrefixOf`) <$> cmdLines "rpm" ["-q", "--provides", sysrelpkg]
+          case msysrelease of
+            Just sysrelease ->
+              -- "system-release(40)"
+              return $ init $ dropPrefix prefix sysrelease
+            Nothing -> error' $ "failed to determine" +-+ systemRelease
+        Nothing -> error' "failed to determine OS version"
+
+getRpmOSName :: IO String
+getRpmOSName = do
+  let osrelease = "/etc/os-release"
+      idkey = "ID="
+  osids <- grep ('^' : idkey) osrelease
+  case osids of
+    [] -> error' $ "failed to find ID in" +-+ osrelease
+    [osid] -> return $
+              dropSuffix "\"" $
+              dropPrefix "\"" $
+              dropPrefix idkey osid
+    oss -> error' $ "multiple IDs in" +-+ osrelease ++ ":" ++ unwords oss
--- src/CoprRepo.hs
+++ src/CoprRepo.hs
@@ -7,11 +7,12 @@ import Control.Monad.Extra
 import Data.List.Extra
 import Network.Curl (curlGetString, CurlCode(CurlOK))
 import Network.HTTP.Directory ((+/+))
-import SimpleCmd (cmdLines, cmdMaybe, error', grep, warning, (+-+))
+import SimpleCmd (error', warning, (+-+))
 import System.Directory (doesFileExist)
 import System.FilePath
 import System.IO.Extra (withTempDir)
 
+import Common
 import Sudo
 
 fedoraCopr :: String
@@ -55,41 +56,3 @@ addCoprRepo dryrun debug mosname mrelease repo = do
             else error' $ "unknown copr server:" +-+ rpo
           ["copr",_,_,_] -> serverOwnerProject $ dropPrefix "copr:" rpo
           _ -> error' $ "unknown copr:" +-+ rpo
-
-getRpmOsRelease :: IO String
-getRpmOsRelease = do
-  -- not defined for fedora branches
-  let systemReleaseVer = "system-release(releasever)"
-  mReleaseVerPkg <- cmdMaybe "rpm" ["-q", "--whatprovides", systemReleaseVer]
-  case mReleaseVerPkg of
-    Just relverPkg -> do
-      mreleasever <- find (systemReleaseVer `isPrefixOf`) <$> cmdLines "rpm" ["-q", "--provides", relverPkg]
-      case mreleasever of
-        Just releasever -> return $ last (words releasever)
-        Nothing -> error' $ "failed to determine" +-+ systemReleaseVer
-    Nothing -> do
-      let systemRelease = "system-release"
-      msysreleasepkg <- cmdMaybe "rpm" ["-q", "--whatprovides", systemRelease]
-      case msysreleasepkg of
-        Just sysrelpkg -> do
-          let prefix = systemRelease ++ "("
-          msysrelease <- find (prefix `isPrefixOf`) <$> cmdLines "rpm" ["-q", "--provides", sysrelpkg]
-          case msysrelease of
-            Just sysrelease ->
-              -- "system-release(40)"
-              return $ init $ dropPrefix prefix sysrelease
-            Nothing -> error' $ "failed to determine" +-+ systemRelease
-        Nothing -> error' "failed to determine OS version"
-
-getRpmOSName :: IO String
-getRpmOSName = do
-  let osrelease = "/etc/os-release"
-      idkey = "ID="
-  osids <- grep ('^' : idkey) osrelease
-  case osids of
-    [] -> error' $ "failed to find ID in" +-+ osrelease
-    [osid] -> return $
-              dropSuffix "\"" $
-              dropPrefix "\"" $
-              dropPrefix idkey osid
-    oss -> error' $ "multiple IDs in" +-+ osrelease ++ ":" ++ unwords oss
